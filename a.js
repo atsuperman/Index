3edd8ed5f24c912f164aa24f2b7e51fcf313c296
atsuperman <smstarvinoth@gmail.com>
2024-05-12 12:05:24 +0530

    a.js

--- a.js
+++ a.js
@@ -1,1 +1,28 @@
 
+// a.js
+
+// Set trackUrls as a global variable
+const trackUrls = [
+  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Boom Chakala .mp3?v=1714923647324",
+  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Thannanthani kattukulla .mp3?v=1714915753431",
+  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Theendai mei theendai .mp3?v=1714915698132",
+  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Moonu mulam .mp3?v=1714915650285",
+  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Annakili nee vadi .mp3?v=1714915594186",
+  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Annakikli nee 2 .mp3?v=1714915565594",
+  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Ponnu oruthi chumma .mp3?v=1714915673759",
+  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Madha Madhannu .mp3?v=1714915547924",
+  "https://cdn.glitch.global/fdbaff0e-6af5-4a23-87cc-fb59b12fec81/🎼 Vazha Thoppukulla .mp3?v=1714901739948",
+  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Lajjavathiye .mp3?v=1714888562865",
+  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Unthan Vizhi Munai 💿 4 student's.mp3?v=1714884720589",
+];
+
+// Function to shuffle the array
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+
