91444da8a6c70c1b0862391ae4c8588b50f8254e
atsuperman <smstarvinoth@gmail.com>
2024-05-15 21:08:06 +0530

    a.js

--- a.js
+++ a.js
@@ -1,29 +1,26 @@
-
-// a.js
-
-// Set trackUrls as a global variable
-const trackUrls = [
-  "https://github.com/atsuperman/Index/raw/main/vazha%20thoppukulla.mp3",
-  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Boom Chakala .mp3?v=1714923647324",
-  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Thannanthani kattukulla .mp3?v=1714915753431",
-  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Theendai mei theendai .mp3?v=1714915698132",
-  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Moonu mulam .mp3?v=1714915650285",
-  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Annakili nee vadi .mp3?v=1714915594186",
-  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Annakikli nee 2 .mp3?v=1714915565594",
-  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Ponnu oruthi chumma .mp3?v=1714915673759",
-  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Madha Madhannu .mp3?v=1714915547924",
-  "https://cdn.glitch.global/fdbaff0e-6af5-4a23-87cc-fb59b12fec81/🎼 Vazha Thoppukulla .mp3?v=1714901739948",
-  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Lajjavathiye .mp3?v=1714888562865",
-  "https://cdn.glitch.global/1780eae4-2681-418a-b256-59d3398b90a7/🎼 Unthan Vizhi Munai 💿 4 student's.mp3?v=1714884720589",
+// Array of video URLs in the playlist
+var playlist = [
+    "https://example.com/video1.mp4",
+    "https://example.com/video2.mp4",
+    "https://example.com/video3.mp4"
 ];
 
-// Function to shuffle the array
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
+// Get the video player element
+var videoPlayer = document.getElementById("videoPlayer");
+
+// Index to keep track of the current video being played
+var currentVideoIndex = 0;
+
+// Function to play the next video in the playlist
+function playNextVideo() {
+    currentVideoIndex = (currentVideoIndex + 1) % playlist.length;
+    videoPlayer.src = playlist[currentVideoIndex];
+    videoPlayer.play();
 }
 
+// Play the first video when the page loads
+videoPlayer.src = playlist[currentVideoIndex];
+videoPlayer.play();
 
+// Event listener to play the next video when the current one ends
+videoPlayer.addEventListener("ended", playNextVideo);
